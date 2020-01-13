d070c56ca7b150edd457951311158653ad0ae43c
KozmoRyder <lautarobiondi@hotmail.com>
2020-01-13 17:44:23 -0300

    V0.6

--- proyecto-js/js/main.js
+++ proyecto-js/js/main.js
@@ -1,14 +1,17 @@
 $(document).ready(function () {
 
     //Slider
-    $('.galeria').bxSlider({
-        mode: 'fade',
-        captions: true,
-        slideWidth: 1200,
-        responsive: true
-    });
+    if(window.location.href.indexOf('index') > -1){
+        $('.galeria').bxSlider({
+            mode: 'fade',
+            captions: true,
+            slideWidth: 1200,
+            responsive: true
+        });
+    }
 
     //Posts
+    if(window.location.href.indexOf('index') > -1){
     var posts = [
         {
             title: 'Prueba de titulo 1',
@@ -55,6 +58,101 @@ $(document).ready(function () {
             sunt in culpa qui officia deserunt mollit anim id est laborum.
         `}
     ]
-    console.log(posts)
+    
+    posts.forEach((item, index) => {
+        var post =`
+        <article class="post">
+        <h2>${item.title}</h2>
+        <span class="date">${item.date}</span>
+        <p>${item.content}</p>
+        <a href="#" class="button-more">Leer mas</a>    
+    </article>`;
+    $('#posts').append(post);
+    });
+    }   
+    //Selector de tema
+     var theme= $('#theme');
+     $('#to-green').click(function(){
+         theme.attr("href",'css/green.css')
+     })
+
+     $('#to-red').click(function(){
+         theme.attr("href",'css/red.css')
+     })
+
+     $('#to-blue').click(function(){
+         theme.attr("href",'css/blue.css')
+     });
+
+     //Scroll arriba
+     $('#subir').click(function(){
+        $('html, body').animate({
+            scrollTop: 0
+        },500)
+     });
+
+     //login falso 
+     $('#login form').submit(function(){
+        var form_name = $("#form_name").val();
+        localStorage.setItem("form_name",form_name);
+     });
+
+     var form_name = localStorage.getItem("form_name");
+     if(form_name!=null && form_name!="undefined"){
+        var about_parrafo=$("#about p")
+        $("#about p").html("<br/><strong>Bienvenido, "+form_name+"</strong>");
+        about_parrafo.append("<a href='#' id='logout'>Cerrar Sesion</a>")
+        $("#login").hide();
+     };
+
+     $('#logout').click(function(){
+         localStorage.clear();
+         location.reload();
+     })
+ 
+//Acordeon
+if(window.location.href.indexOf('about') > -1){
+    $("#acordeon").accordion();
+}
+
+//Reloj
+if(window.location.href.indexOf('reloj') > -1){
+
+    setInterval(function(){
+        var reloj = moment().format('h:mm:ss')
+        $('#reloj').html(reloj);
+    },1000);
+}
+
+//validacion
+if(window.location.href.indexOf('contacto') > -1){
+
+    $("form input[name='date']").datepicker({
+        dateFormat: 'dd-mm-yy'
+    });
+
+
+    $.validate({
+    lang: 'es',
+    errorMessagePosition: 'top',
+    scrollToTopOnError: true
+  });
+}
+
+
+//Ver fondo
+
+$('#ver-fondo').click(function(){
+    if ($('#ver-fondo').text() == 'ver fondo'){
+        $('#ver-fondo').text('ocultar fondo');
+        $('#global, footer').hide();
+    }else{
+        $('#ver-fondo').text('ver fondo');
+        $('#global, footer').show();
+    }
+    
+})
+
+
 });
 
