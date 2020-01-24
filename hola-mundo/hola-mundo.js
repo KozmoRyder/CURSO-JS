0c0bd80aa1bebc7e2141d7cc29288f23158452b9
KozmoRyder <lautarobiondi@hotmail.com>
2020-01-24 17:47:43 -0300

    V1.2

--- hola-mundo/hola-mundo.js
+++ hola-mundo/hola-mundo.js
@@ -3,3 +3,9 @@ console.log('Hola Mundo en JS')
 
 console.log('Hola Mundo en JS')// Esto es un console.log()
 
+
+var resultado=Number;
+function obtenerValor(numero1,numero2){
+    resultado=numero1+numero2;
+    console.log(resultado);
+}
